21aa39b7fc1958d8a45c0749057f115e91512d39
theabsparrow <absbashar04@gmail.com>
2024-02-19 21:25:37 +0600

    coupon section and grand total section complete

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const byeTickets = document.getElementById("bye-tickets");
+let totalPrice = 0;
+let grandTotalPrice = 0;
+let grandTotal = document.getElementById("grand-total");
 byeTickets.addEventListener('click', function () {
     const selectYourSeat = document.getElementById("select-your-seat");
     selectYourSeat.scrollIntoView();
@@ -18,7 +21,7 @@ for (i = 0; i < busSeats.length; i++) {
         const number = parseInt(fourtySeatsNumber);
         const newNumber = number - 1;
         fourtySeatsLeft.innerText = newNumber;
-        
+
         // select seats section
         const selectedSeat = document.getElementById("selected-seat");
         const selectedSeatNumber = selectedSeat.innerText;
@@ -39,7 +42,47 @@ for (i = 0; i < busSeats.length; i++) {
         div.appendChild(p2);
         div.appendChild(p3);
         seatAndPrice.appendChild(div);
+
+        const ticketPrice = 550;
+
+        // total amount section
+        const totalAmount = document.getElementById("total-amount");
+        totalPrice += ticketPrice;
+        totalAmount.innerText = totalPrice;
+
+        // grand total amount section
+        const grandTotal = document.getElementById("grand-total");
+        grandTotalPrice += ticketPrice;
+        grandTotal.innerText = grandTotalPrice;
+
     })
-    
-    
 }
+
+// coupon section
+const couponButton = document.getElementById("coupon-button");
+couponButton.addEventListener('click', function () {
+    const couponInput = document.getElementById("coupon-input");
+    const couponElement = couponInput.value;
+    const couponCode = couponElement;
+    couponInput.value = "Have any coupon"
+    // first coupon section
+    if (totalPrice >= 1100 && couponCode === "NEW15") {
+
+        const discountAmount = totalPrice * 0.15;
+        const discountGrandTotal = totalPrice - discountAmount;
+        grandTotal.innerText = discountGrandTotal;
+        return;
+    }
+    else if (totalPrice === 2200 && couponCode === "Couple 20") {
+
+        const discount = totalPrice * 0.2
+        const coupleDiscount = totalPrice - discount;
+        grandTotal.innerText = coupleDiscount;
+        return;
+    }
+    else {
+        alert("bye atleast two tickets or invalid coupon code");
+    }
+})
+
+
