e3949a70127a0484f719774393074d217e10809e
theabsparrow <absbashar04@gmail.com>
2024-02-19 18:29:44 +0600

    seat section added

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ const busSeats = document.getElementsByClassName("bus-seat");
 for (i = 0; i < busSeats.length; i++) {
     const busSeat = busSeats[i];
     busSeat.addEventListener('click', function () {
-        const name = busSeat.innerText;
         busSeat.style.backgroundColor = "#1DD100"
+        const name = busSeat.innerText;
+
+        // fourty seat left
+        const fourtySeatsLeft = document.getElementById("fourty-seats-left");
+        const fourtySeatsNumber = fourtySeatsLeft.innerText;
+        const number = parseInt(fourtySeatsNumber);
+        const newNumber = number - 1;
+        fourtySeatsLeft.innerText = newNumber;
         
+        // select seats section
+        const selectedSeat = document.getElementById("selected-seat");
+        const selectedSeatNumber = selectedSeat.innerText;
+        const seatNumber = parseInt(selectedSeatNumber);
+        const newSeatNumber = seatNumber + 1;
+        selectedSeat.innerText = newSeatNumber;
+
+        // seat added section
+        const seatAndPrice = document.getElementById("seat-and-price");
+        const div = document.createElement("div")
+        const p = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+        p.innerText = name;
+        p2.innerText = "Economy";
+        p3.innerText = "550";
+        div.appendChild(p);
+        div.appendChild(p2);
+        div.appendChild(p3);
+        seatAndPrice.appendChild(div);
     })
     
     
