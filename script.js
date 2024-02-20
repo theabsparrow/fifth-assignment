6b26901527f3073aa109d6e08b5f41c7fa300058
theabsparrow <absbashar04@gmail.com>
2024-02-20 10:54:52 +0600

    final shot

--- script.js
+++ script.js
@@ -19,26 +19,32 @@ for (i = 0; i < busSeats.length; i++) {
         busSeat.style.backgroundColor = "#1DD100"
         const name = busSeat.innerText;
         j++
-        console.log(j);
+        
         if (selectedBusSeat.includes(name) === false) {
             selectedBusSeat.push(name);
-            console.log(selectedBusSeat);
-            if (selectedBusSeat.length <= 4) {
-                // fourty seat left
+            
+            if (selectedBusSeat.length > 4) {
+                alert ("you can't bye more than four tickets together");
+                return;
+                
+            }
+            else{
+
+                // total seats decreasing section
                 const fourtySeatsLeft = document.getElementById("fourty-seats-left");
                 const fourtySeatsNumber = fourtySeatsLeft.innerText;
                 const number = parseInt(fourtySeatsNumber);
                 const newNumber = number - 1;
                 fourtySeatsLeft.innerText = newNumber;
 
-                // select seats section
+                // select seats increasing section
                 const selectedSeat = document.getElementById("selected-seat");
                 const selectedSeatNumber = selectedSeat.innerText;
                 const seatNumber = parseInt(selectedSeatNumber);
                 const newSeatNumber = seatNumber + 1;
                 selectedSeat.innerText = newSeatNumber;
 
-                // seat added section
+                // seat number added section
                 const seatAndPrice = document.getElementById("seat-and-price");
                 const div = document.createElement("div")
                 const p = document.createElement("p");
@@ -65,118 +71,20 @@ for (i = 0; i < busSeats.length; i++) {
                 grandTotalPrice += ticketPrice;
                 grandTotal.innerText = grandTotalPrice;
 
-                if(grandTotalPrice === 2200){
-                    alert ("you can't bye more than four ticket at a time");
-                    return;
-                }
             }
         }
     })
 }
-console.log(totalPrice);
-
-
-
-// else {
-//     alert ("no more selection")
-//     return;
-// }
-
-
-// // fourty seat left
-// const fourtySeatsLeft = document.getElementById("fourty-seats-left");
-// const fourtySeatsNumber = fourtySeatsLeft.innerText;
-// const number = parseInt(fourtySeatsNumber);
-// const newNumber = number - 1;
-// fourtySeatsLeft.innerText = newNumber;
-
-// // select seats section
-// const selectedSeat = document.getElementById("selected-seat");
-// const selectedSeatNumber = selectedSeat.innerText;
-// const seatNumber = parseInt(selectedSeatNumber);
-// const newSeatNumber = seatNumber + 1;
-// selectedSeat.innerText = newSeatNumber;
-
-// // seat added section
-// const seatAndPrice = document.getElementById("seat-and-price");
-// const div = document.createElement("div")
-// const p = document.createElement("p");
-// const p2 = document.createElement("p");
-// const p3 = document.createElement("p");
-// p.innerText = name;
-// p2.innerText = "Economy";
-// p3.innerText = "550";
-// div.appendChild(p);
-// div.appendChild(p2);
-// div.appendChild(p3);
-// seatAndPrice.appendChild(div);
-
-// const ticketPrice = 550;
-
-// // total amount section
-// const totalAmount = document.getElementById("total-amount");
-// totalPrice += ticketPrice;
-// totalAmount.innerText = totalPrice;
-
-// // grand total amount section
-// const grandTotal = document.getElementById("grand-total");
-// grandTotalPrice += ticketPrice;
-// grandTotal.innerText = grandTotalPrice;
-
-
-
-
-// coupon section
-// const couponButton = document.getElementById("coupon-button");
-
-// function couponInput() {
-//     const inputCoupon = document.getElementById("coupon-input")
-//     let theField = true;
-//     if (inputCoupon.value.length === 0) {
-//         theField = false
-//     }
-//     else if (theField) {
-//         couponButton.disabled = false;
-//     }
-//     else {
-//         couponButton.disabled = true;
-//     }
-
-// }
-
-// function couponButton() {
-   
-//     const couponElement = inputCoupon.value;
-//     const couponCode = couponElement;
-//     inputCoupon.value = "Have any coupon"
-
-//     if (totalPrice >= 1100 && couponCode === "NEW15") {
-
-//         const discountAmount = totalPrice * 0.15;
-//         const discountGrandTotal = totalPrice - discountAmount;
-//         grandTotal.innerText = discountGrandTotal;
-//         return;
-//     }
-//     else if (totalPrice === 2200 && couponCode === "Couple 20") {
-
-//         const discount = totalPrice * 0.2
-//         const coupleDiscount = totalPrice - discount;
-//         grandTotal.innerText = coupleDiscount;
-//         return;
-//     }
-//     else {
-//         alert("bye atleast two tickets or invalid coupon code");
-//     }
-// }
-
 
 const couponButton = document.getElementById("coupon-button");
 couponButton.addEventListener('click', function () {
     const couponInput = document.getElementById("coupon-input");
     const couponElement = couponInput.value;
     const couponCode = couponElement;
-    couponInput.value = "Have any coupon"
+    couponInput.value = ""
+    
     // coupon section
+
     if (totalPrice >= 1100 && couponCode === "NEW15") {
 
         const discountAmount = totalPrice * 0.15;
@@ -196,20 +104,6 @@ couponButton.addEventListener('click', function () {
     }
 })
 
-// info section
-
-// const passengerName = document.getElementById ("passenger-name");
-// const Name = passengerName.value;
-// const nameValue = Name;
-
-// const passengerPhoneNumber = document.getElementById("passenger-number");
-// const numberPass = passengerPhoneNumber.value;
-// const numberValue = numberPass;
-
-// const passengerEmailId = document.getElementById("passenger-email");
-// const emailId = passengerEmailId.value;
-// const emailIdValue = emailId;
-
 const nextButton = document.getElementById("next-button");
 
 function checkform() {
@@ -220,7 +114,7 @@ function checkform() {
             fieldIsFiiled = false;
         }
     }
-    if (fieldIsFiiled) {
+    if ((fieldIsFiiled) && (selectedBusSeat.length > 0) ) {
         nextButton.disabled = false;
 
     }
